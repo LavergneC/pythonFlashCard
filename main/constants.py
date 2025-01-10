88032ab2f0c69a56fb48866f7d33333ae22d3ec4
Lavergne Clement <lavergne.clement@yahoo.fr>
2025-01-10 12:22:17 +0100

    Text edit

--- main/constants.py
+++ main/constants.py
@@ -1,12 +1,14 @@
 TEXT_TESTS_PART = (
     "# The following section should not be modified.\n"
     "# It contains all the tests used to validate your response\n"
+    "# Test your solution by running '$ pytest exercise.py'\n"
 )
 TEXT_USER_CODE = """\
     ##################
     # Your code here #
     ##################\
 """
+TEXT_PYTEST = "# Test your solution by running '$ pytest'"
 
 KEEP_LINE_TAG = "# fcPython:keep line"
 
